c2f6b538d39e2eb1fe8b76351edb095c882e695e
saitejamalladi <saiteja.malladi@gmail.com>
2020-12-26 00:15:33 +0800

    basic layout

--- src/containers/Canvas/Canvas.js
+++ src/containers/Canvas/Canvas.js
@@ -1,63 +1,59 @@
 import React, {Component} from 'react';
-import {Button} from '@material-ui/core';
+import {Layer, Rect, Stage} from 'react-konva';
+import classes from './Canvas.module.css';
+
+const Konva= window.Konva;
 
 class Canvas extends Component {
-	boardWidth = 600;
-	boardHeight = 600;
-
-	state = {
-		boxWidth: 50,
-		boxHeight: 50,
-		x: 0,
-		y: 0
-	}
-	componentDidMount() {
-		this.updateCanvas(10, 10);
+	constructor(...args) {
+		super(...args);
+		this.state = {
+			innerRadius: 50,
+			outerRadius: 150,
+		};
 	}
 
-	updateCanvas = (newX, newY) => {
-		let currentX = this.state.x;
-		let currentY = this.state.y;
-		if(newX < 0 || newX > this.boardWidth) newX = currentX;
-		if(newY < 0 || newY > this.boardHeight) newY = currentY;
-
-		const ctx = this.refs.canvas.getContext('2d');
-		ctx.clearRect(currentX, currentY, this.state.boxWidth, this.state.boxHeight);
+	handleClick = () => {
 		this.setState({
-			x: newX,
-			y: newY
-		})
-		ctx.fillRect(newX, newY, this.state.boxWidth, this.state.boxHeight);
+			color: Konva.Util.getRandomColor()
+		});
 	}
-	shift = (xStep, yStep) => {
-		this.updateCanvas(this.state.x + xStep, this.state.y + yStep);
-	}
-	handleReset = () =>  {
-		this.updateCanvas(this.boardWidth/2 - 5, this.boardHeight/2 - 5);
-	}
+
 	render () {
 		return (
-			<div>
-				<div>
-					x : {this.state.x}
-					<br/>
-					y : {this.state.y}
-				</div>
-				<div>
-					<Button variant={"contained"} color={"secondary"} onClick={this.handleReset}>Reset</Button>
-					<Button variant={"contained"} onClick={() => this.shift(-50,  0)}>Left</Button>
-					<Button variant={"contained"} onClick={() => this.shift(+50,0)}>Right</Button>
-					<Button variant={"contained"} onClick={() => this.shift(0, -50)}>Up</Button>
-					<Button variant={"contained"} onClick={() => this.shift(0, +50)}>Down</Button>
-					<br />
-					<Button variant={"contained"} onClick={() => this.shift(-50, -50)}>Top Left</Button>
-					<Button variant={"contained"} onClick={() => this.shift(+50, -50)}>Top Right</Button>
-					<Button variant={"contained"} onClick={() => this.shift(-50, +50)}>Bottom Left</Button>
-					<Button variant={"contained"} onClick={() => this.shift(+50, +50)}>Bottom Right</Button>
-				</div>
-				<div>
-					<canvas ref="canvas" width={this.boardWidth} height={this.boardHeight}/>
-				</div>
+			<div className={classes.Canvas}>
+			<Stage width={350} height={350}>
+				<Layer>
+					<Rect
+						x={0}
+						y={0}
+						width={350}
+						height={350}
+						fill={"#b9ada1"}
+						onDragEnd={() =>
+							this.handleClick
+						}
+					/>
+					{Array.from(Array(4).keys()).map((row, rowIndex) => {
+						return Array.from(Array(4).keys()).map((col, colIndex) => {
+							return (
+							<Rect
+									key={rowIndex + " " + colIndex}
+									x={10 * (colIndex+1) + 75 * (colIndex)}
+									y={10 * (rowIndex+1) + 75 * (rowIndex)}
+									width={75}
+									height={75}
+									fill={"#ebe0cb"}
+									onDragEnd={() =>
+										this.handleClick
+									}
+								/>
+							)
+						}
+					)})
+					}
+				</Layer>
+			</Stage>
 			</div>
 		)
 	}
